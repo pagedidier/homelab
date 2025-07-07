bbd52f97e2f5286e20d091aa260b9c37cc3ea28c
Didier Page <didier.page@arcanite.ch>
2025-07-07 22:34:35 +0200

    Remove useless provider

--- terraform/infra/providers.tf
+++ terraform/infra/providers.tf
@@ -1,29 +1,9 @@
 terraform {
   backend "http" {}
   required_providers {
-    mysql = {
-      source = "petoju/mysql"
-      version = "3.0.78"
-    }
-    haproxy = {
-      source = "SepehrImanian/haproxy"
-      version = "0.0.7"
-    }
     proxmox = {
       source = "bpg/proxmox"
       version = "0.61.1"
-    }
-    gitlab = {
-      source = "gitlabhq/gitlab"
-      version = "17.0.1"
-    }
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.31.0"
-    }
-    dns = {
-      source = "hashicorp/dns"
-      version = "3.4.3"
     }
   }
 }
@@ -39,11 +19,3 @@ provider "proxmox" {
     private_key = file(var.proxmox_ssh_key_path)
   }
 }
-
-provider "kubernetes" {
-  config_path    = "~/.kube/config"
-}
-# provider "vault" {
-#    address = "https://vault.twop.ch"
-#   token = var.vault_token
-# }
