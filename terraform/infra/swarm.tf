54d3ae20ab3058b87b9919521a029d34d91cc15a
Didier Page <didier.page@arcanite.ch>
2025-08-25 18:48:34 +0200

    Add docker swarm config

--- terraform/infra/swarm.tf
+++ terraform/infra/swarm.tf
@@ -61,3 +61,36 @@ resource "infomaniak_record" "swarm-node3" {
   ttl = 300
   target = module.swarm-node3.vm.ip
 }
+
+module "swarm-node11" {
+  source = "./modules/vm"
+  name   = "swarm-11.prod"
+  node_name = data.proxmox_virtual_environment_node.node21.node_name
+  init_ssh_keys = var.init_ssh_keys
+  init_user_password = var.init_user_password
+  init_user_username = var.init_user_username
+  ram_in_bytes = 4096
+  ip="192.168.1.48/16"
+  gateway = "192.168.1.1"
+  agent_enable = true
+  image_storage_name = "local"
+  providers = {
+    proxmox = proxmox.pvc02,
+  }
+}
+
+resource "infomaniak_record" "swarm-node11" {
+  zone_name = data.infomaniak_zone.twop.fqdn
+  source = module.swarm-node11.vm.hostname
+  type = "A"
+  ttl = 300
+  target = module.swarm-node11.vm.ip
+}
+
+resource "infomaniak_record" "swarm-node11-plex" {
+  zone_name = data.infomaniak_zone.nohanbudry.fqdn
+  source = "plex"
+  type = "A"
+  ttl = 300
+  target = module.swarm-node11.vm.ip
+}
