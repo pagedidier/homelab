abc591e13ea6ae32c54656ceb2b8b8575b99bbf2
Didier Page <didier.page@arcanite.ch>
2025-08-25 18:49:04 +0200

    Add nomand cluster

---
+++ terraform/infra/nomad.tf
@@ -0,0 +1,41 @@
+module "nomad-server01" {
+  source = "./modules/vm"
+  name   = "nomad-server01.prod"
+  node_name = data.proxmox_virtual_environment_node.node12.node_name
+  init_ssh_keys = var.init_ssh_keys
+  init_user_password = var.init_user_password
+  init_user_username = var.init_user_username
+  ram_in_bytes = 4096
+  ip="192.168.0.131/16"
+  gateway = "192.168.0.254"
+  agent_enable = true
+}
+
+resource "infomaniak_record" "nomad-server01" {
+  zone_name = data.infomaniak_zone.twop.fqdn
+  source = module.nomad-server01.vm.hostname
+  type = "A"
+  ttl = 300
+  target = module.nomad-server01.vm.ip
+}
+
+module "nomad-client01" {
+  source = "./modules/vm"
+  name   = "nomad-client01.prod"
+  node_name = data.proxmox_virtual_environment_node.node12.node_name
+  init_ssh_keys = var.init_ssh_keys
+  init_user_password = var.init_user_password
+  init_user_username = var.init_user_username
+  ram_in_bytes = 4096
+  ip="192.168.0.141/16"
+  gateway = "192.168.0.254"
+  agent_enable = true
+}
+
+resource "infomaniak_record" "nomad-client01" {
+  zone_name = data.infomaniak_zone.twop.fqdn
+  source = module.nomad-client01.vm.hostname
+  type = "A"
+  ttl = 300
+  target = module.nomad-client01.vm.ip
+}
