2f025e26567fc60ad1093d23e27951059834db05
Didier Page <didier.page@arcanite.ch>
2025-09-04 17:15:42 +0200

    Add install script for swarm and fix folder name

---
+++ swarm/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SWARM_CLUSTER_NAME=$1
+STACK_NAME=$2
+
+if [ "$SWARM_CLUSTER_NAME" = "" ]; then echo "You need to pass swarm cluster name" && echo " $0 <swarm_cluster_name> <stack_name>" && exit 1; fi
+if [ "$STACK_NAME" = "" ]; then echo "You need to pass stack name" && echo " $0 <swarm_cluster_name> <stack_name>" && exit 1; fi
+
+
+docker stack deploy -c $SWARM_CLUSTER_NAME/$STACK_NAME/compose.yml $STACK_NAME
