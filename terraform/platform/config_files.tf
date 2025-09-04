c9406b195c0f9c1a7675713164bd7aee07b8e754
Didier Page <didier.page@arcanite.ch>
2025-09-04 21:34:56 +0200

    Add config for vault

---
+++ terraform/platform/config_files.tf
@@ -0,0 +1,9 @@
+resource "local_file" "rendered_file" {
+  filename = "../../swarm/swarm01/vault/config.hcl"
+  content  = templatefile("../../swarm/swarm01/vault/config.hcl.tmpl", {
+    endpoint = var.database["database.prod"].endpoint
+    username  = module.vault-database.user
+    password = module.vault-database.password
+    database =  module.vault-database.database
+  })
+}
