c42fa2f79aa8a7d5e20d56bd7f40b7c5346a5527
Didier Page <didier.page@arcanite.ch>
2025-09-20 20:15:01 +0200

    Allow forwarding registry credentials to docker swarm

--- swarm/install.sh
+++ swarm/install.sh
@@ -7,4 +7,4 @@ if [ "$SWARM_CLUSTER_NAME" = "" ]; then echo "You need to pass swarm cluster nam
 if [ "$STACK_NAME" = "" ]; then echo "You need to pass stack name" && echo " $0 <swarm_cluster_name> <stack_name>" && exit 1; fi
 
 
-docker stack deploy -c $SWARM_CLUSTER_NAME/$STACK_NAME/compose.yml $STACK_NAME
+docker stack deploy --with-registry-auth -c $SWARM_CLUSTER_NAME/$STACK_NAME/compose.yml $STACK_NAME
