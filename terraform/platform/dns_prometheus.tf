33c65263857b47e8107db5886487d4c8a144afb8
Didier Page <didier.page@arcanite.ch>
2025-10-25 17:07:46 +0200

    Import k3s and monitoring SRV dns

--- terraform/platform/dns_prometheus.tf
+++ terraform/platform/dns_prometheus.tf
@@ -46,6 +46,72 @@ module "node_exporter_proxmox13" {
   port = 9100
 }
 
+module "node_exporter_proxy01" {
+  source = "./modules/dns/srv"
+  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
+  dns_source = "_node-exporter"
+  target = "proxy01.twop.ch"
+  port = 9100
+}
+
+module "node_exporter_swarm01" {
+  source = "./modules/dns/srv"
+  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
+  dns_source = "_node-exporter"
+  target = "swarm-01.prod.twop.ch"
+  port = 9100
+}
+
+module "node_exporter_swarm02" {
+  source = "./modules/dns/srv"
+  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
+  dns_source = "_node-exporter"
+  target = "swarm-02.prod.twop.ch"
+  port = 9100
+}
+
+module "node_exporter_swarm03" {
+  source = "./modules/dns/srv"
+  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
+  dns_source = "_node-exporter"
+  target = "swarm-03.prod.twop.ch"
+  port = 9100
+}
+
+module "node_exporter_k3s-01-prod" {
+  source = "./modules/dns/srv"
+  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
+  dns_source = "_node-exporter"
+  target = "k3s-01.prod.twop.ch"
+  port = 9100
+}
+
+module "node_exporter_k3s-02-prod" {
+  source = "./modules/dns/srv"
+  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
+  dns_source = "_node-exporter"
+  target = "k3s-02.prod.twop.ch"
+  port = 9100
+}
+
+module "node_exporter_k3s-03-prod" {
+  source = "./modules/dns/srv"
+  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
+  dns_source = "_node-exporter"
+  target = "k3s-03.prod.twop.ch"
+  port = 9100
+}
+
+module "node_exporter_glrunner" {
+  source = "./modules/dns/srv"
+  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
+  dns_source = "_node-exporter"
+  target = "glrunner.twop.ch"
+  port = 9100
+}
+
+
+
 module "ceph_exporter_proxmox11" {
   source = "./modules/dns/srv"
   zone_fqdn = infomaniak_zone._tcp_twop.fqdn
