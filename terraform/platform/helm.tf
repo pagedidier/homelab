15e009d9f86113f46c3a1664b1a7ebd632b63f1e
Didier Page <didier.page@arcanite.ch>
2025-12-02 08:16:39 +0100

    Add helm config to configure vault and dashboard in k8s

---
+++ terraform/platform/helm.tf
@@ -0,0 +1,58 @@
+resource "helm_release" "kubernetes_dashboard" {
+  name       = "kubernetes-dashboard"
+  repository = "https://kubernetes.github.io/dashboard/"
+  chart      = "kubernetes-dashboard"
+
+  namespace  = "kubernetes-dashboard"
+  create_namespace = true
+}
+
+resource "kubernetes_service_account" "admin_user" {
+  metadata {
+    name      = "admin-user"
+    namespace = "kubernetes-dashboard"
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "admin_user" {
+  metadata {
+    name = "admin-user"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.admin_user.metadata[0].name
+    namespace = kubernetes_service_account.admin_user.metadata[0].namespace
+  }
+}
+
+resource "helm_release" "vault" {
+  name       = "vault"
+  repository = "https://helm.releases.hashicorp.com"
+  chart      = "vault"
+  namespace  = "vault"
+  create_namespace = true
+
+  set {
+    name  = "global.externalVaultAddr"
+    value = "http://192.168.0.111:8300"
+  }
+}
+
+resource "kubernetes_secret" "vault_token" {
+  metadata {
+    name = "vault-token-g955r"
+    namespace  = "vault"
+    annotations = {
+      "kubernetes.io/service-account.name" = "vault"
+    }
+  }
+
+  type = "kubernetes.io/service-account-token"
+}
