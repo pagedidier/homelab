e14c3ba338c6b7f8e1838fe69a87d75e8813d8c6
Didier Page <didier.page@arcanite.ch>
2025-12-11 11:39:10 +0100

    Use ansible role mapping to configure DNS entries for exporters

--- terraform/platform/dns_prometheus.tf
+++ terraform/platform/dns_prometheus.tf
@@ -1,161 +1,58 @@
-module "graphite_exporter_proxmox11" {
+locals {
+  inventory = yamldecode(file("${path.module}/inventory.yaml"))
+}
+
+module "dns_twop_node_exporter" {
+  for_each   = toset(local.inventory["roles"]["node_exporter"])
+  source = "./modules/dns/srv"
+  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
+  dns_source = "_node-exporter"
+  target = each.key
+  port = 9100
+}
+
+module "dns_twop_graphite_exporter" {
+  for_each   = toset(local.inventory["roles"]["graphite_exporter"])
   source = "./modules/dns/srv"
   zone_fqdn = infomaniak_zone._tcp_twop.fqdn
   dns_source = "_graphite-exporter"
-  target = "proxmox11.twop.ch"
+  target = each.key
   port = 9108
 }
 
-module "graphite_exporter_proxmox12" {
-  source = "./modules/dns/srv"
-  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
-  dns_source = "_graphite-exporter"
-  target = "proxmox12.twop.ch"
-  port = 9108
-}
-
-module "graphite_exporter_proxmox13" {
-  source = "./modules/dns/srv"
-  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
-  dns_source = "_graphite-exporter"
-  target = "proxmox13.twop.ch"
-  port = 9108
-}
-
-module "node_exporter_proxmox11" {
-  source = "./modules/dns/srv"
-  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
-  dns_source = "_node-exporter"
-  target = "proxmox11.twop.ch"
-  port = 9100
-}
-
-module "node_exporter_proxmox12" {
-  source = "./modules/dns/srv"
-  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
-  dns_source = "_node-exporter"
-  target = "proxmox12.twop.ch"
-  port = 9100
-}
-
-module "node_exporter_proxmox13" {
-  source = "./modules/dns/srv"
-  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
-  dns_source = "_node-exporter"
-  target = "proxmox13.twop.ch"
-  port = 9100
-}
-
-module "node_exporter_proxy01" {
-  source = "./modules/dns/srv"
-  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
-  dns_source = "_node-exporter"
-  target = "proxy01.twop.ch"
-  port = 9100
-}
-
-module "node_exporter_swarm01" {
-  source = "./modules/dns/srv"
-  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
-  dns_source = "_node-exporter"
-  target = "swarm-01.prod.twop.ch"
-  port = 9100
-}
-
-module "node_exporter_swarm02" {
-  source = "./modules/dns/srv"
-  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
-  dns_source = "_node-exporter"
-  target = "swarm-02.prod.twop.ch"
-  port = 9100
-}
-
-module "node_exporter_swarm03" {
-  source = "./modules/dns/srv"
-  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
-  dns_source = "_node-exporter"
-  target = "swarm-03.prod.twop.ch"
-  port = 9100
-}
-
-module "node_exporter_k3s-01-prod" {
-  source = "./modules/dns/srv"
-  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
-  dns_source = "_node-exporter"
-  target = "k3s-01.prod.twop.ch"
-  port = 9100
-}
-
-module "node_exporter_k3s-02-prod" {
-  source = "./modules/dns/srv"
-  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
-  dns_source = "_node-exporter"
-  target = "k3s-02.prod.twop.ch"
-  port = 9100
-}
-
-module "node_exporter_k3s-03-prod" {
-  source = "./modules/dns/srv"
-  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
-  dns_source = "_node-exporter"
-  target = "k3s-03.prod.twop.ch"
-  port = 9100
-}
-
-module "node_exporter_glrunner" {
-  source = "./modules/dns/srv"
-  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
-  dns_source = "_node-exporter"
-  target = "glrunner.twop.ch"
-  port = 9100
-}
-
-
-
-module "ceph_exporter_proxmox11" {
+module "dns_twop_ceph_exporter" {
+  for_each   = toset(concat(local.inventory["roles"]["ceph_exporter"],local.inventory["roles"]["microceph_exporter"]))
   source = "./modules/dns/srv"
   zone_fqdn = infomaniak_zone._tcp_twop.fqdn
   dns_source = "_ceph-exporter"
-  target = "proxmox11.twop.ch"
+  target = each.key
   port = 9128
 }
 
-module "ceph_exporter_proxmox12" {
+
+module "dns_twop_cadvisor" {
+  for_each   = toset(concat(local.inventory["roles"]["cadvisor/lxc"],local.inventory["roles"]["cadvisor/docker"]))
   source = "./modules/dns/srv"
   zone_fqdn = infomaniak_zone._tcp_twop.fqdn
-  dns_source = "_ceph-exporter"
-  target = "proxmox12.twop.ch"
-  port = 9128
+  dns_source = "_cadvisor"
+  target = each.key
+  port = 8080
 }
 
-module "ceph_exporter_proxmox13" {
-  source = "./modules/dns/srv"
+module "dns_twop_blackbox_exporter" {
+  source = "./modules/dns/srv_list"
   zone_fqdn = infomaniak_zone._tcp_twop.fqdn
-  dns_source = "_ceph-exporter"
-  target = "proxmox13.twop.ch"
-  port = 9128
-}
-
-module "ceph_exporter_swarm01" {
-  source = "./modules/dns/srv"
-  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
-  dns_source = "_ceph-exporter"
-  target = "swarm-01.prod.twop.ch"
-  port = 9128
-}
-
-module "ceph_exporter_swarm02" {
-  source = "./modules/dns/srv"
-  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
-  dns_source = "_ceph-exporter"
-  target = "swarm-02.prod.twop.ch"
-  port = 9128
-}
-
-module "ceph_exporter_swarm03" {
-  source = "./modules/dns/srv"
-  zone_fqdn = infomaniak_zone._tcp_twop.fqdn
-  dns_source = "_ceph-exporter"
-  target = "swarm-03.prod.twop.ch"
-  port = 9128
+  dns_source = "_blackbox-exporter"
+  targets = [
+    "grafana.twop.ch",
+    "audio.twop.ch",
+    "k3s.prod.twop.ch",
+    "nxtcld.twop.ch",
+    "plex.twop.ch",
+    "pvc01.twop.ch",
+    "uptimekuma.twop.ch",
+    "vault.twop.ch",
+    "transmission.twop.ch"
+  ]
+  port = 443
 }
