6247be03b6e7df0abb9fb6307fe8dedcbd8f500d
Didier Page <didier.page@arcanite.ch>
2025-12-11 11:39:10 +0100

    Use ansible role mapping to configure DNS entries for exporters

---
+++ terraform/platform/modules/dns/srv_list/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    infomaniak = {
+      source  = "Infomaniak/infomaniak"
+      version = "1.1.9"
+    }
+  }
+}
+
+resource "infomaniak_record" "srv_record" {
+  for_each = toset(var.targets)
+  zone_fqdn = var.zone_fqdn
+  source = var.dns_source
+  type = "SRV"
+  ttl = var.ttl
+  data = {
+    target = each.key
+    priority = var.priority
+    weight = var.weight
+    port = var.port
+  }
+}
