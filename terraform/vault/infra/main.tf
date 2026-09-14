62da2dc08b7c48e1c12a2f609ffa18a44a4126ea
Didier Page <page.didier@gmail.com>
2026-09-14 20:53:39 +0200

    Add auto-unseal

--- terraform/vault/infra/main.tf
+++ terraform/vault/infra/main.tf
@@ -30,3 +30,43 @@ module "homelab" {
     }
   ]
 }
+
+resource "vault_mount" "transit" {
+  path        = "transit"
+  type        = "transit"
+  description = "Transit secrets engine for auto-unseal"
+}
+
+resource "vault_transit_secret_backend_key" "autounseal" {
+  backend    = vault_mount.transit.path
+  name       = "autounseal"
+  exportable = false
+}
+
+
+data "vault_policy_document" "autounseal-policy-document" {
+  rule {
+    capabilities = ["update"]
+    path         = "transit/encrypt/autounseal"
+  }
+  rule {
+    capabilities = ["update"]
+    path         = "transit/decrypt/autounseal"
+  }
+}
+
+resource "vault_policy" "autounseal-policy" {
+  name   = "autounseal"
+  policy = data.vault_policy_document.autounseal-policy-document.hcl
+}
+
+resource "vault_token" "autounseal" {
+  policies  = [vault_policy.autounseal-policy.name]
+  period    = "24h"
+  no_parent = true
+}
+
+output "autounseal_token" {
+  value     = vault_token.autounseal.client_token
+  sensitive = true
+}
